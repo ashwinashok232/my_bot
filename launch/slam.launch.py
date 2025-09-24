d90c9681a78c133b2d9d59511725ccaaf770c470
Ashwin Ashok <ashwinashok232@gmail.com>
2025-09-24 15:34:19 -0700

    Add slam launch file

---
+++ launch/slam.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='slam_toolbox',
+            executable='sync_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[{
+                'use_sim_time': True,
+                'slam_params_file': '/path/to/slam_params.yaml'
+            }]
+        )
+    ])
